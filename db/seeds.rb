ca7159a8b6e0add5db856ef8b5aeeda77b955c21
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-02-05 02:11:50 -0800

    Implement edit funtionality for the Post status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-
 puts "1 User created"
+
+AdminUser.create(email: "seb@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "seb_admin", last_name: "vez")
+puts "1 AdminUser created"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
