4b0b406f34e9d7fa9164b97a3cc9d1e9c90bf0f3
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-02-13 23:27:41 -0800

    Implement new index query for showing users posts they created and hiding others

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,75 +1,92 @@
-# spec/post_spec.rb
-
 require 'rails_helper'
 
 describe 'navigate' do
-	before do
-		@user = FactoryGirl.create(:user)
-	  login_as(@user, :scope => :user)
-	end
+  before do
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
+  end
 
-	describe 'index' do
-		before do
-			visit posts_path
-		end
+  describe 'index' do
+    before do
+      visit posts_path
+    end
 
-		it 'can be reached successfully' do
-			expect(page.status_code).to eq(200)
-		end
-		it 'has a title of Posts' do
-			expect(page).to have_content(/Posts/)
-		end
-		it 'has a list of posts' do
-			post1 = FactoryGirl.build_stubbed(:post)
-			post2 = FactoryGirl.build_stubbed(:second_post)
-			visit posts_path
-			expect(page).to have_content(/Rationale|content/)
-		end
-	end
+  	it 'can be reached successfully' do
+  		expect(page.status_code).to eq(200)
+  	end
 
-	describe 'new' do
-		it 'has a link from homepage' do
-			visit root_path
-			click_link("new_post_from_nav")
-			expect(page.status_code).to eq(200)
-		end
-	end
+  	it 'has a title of Posts' do
+  		expect(page).to have_content(/Posts/)
+  	end
 
-	describe 'delete' do
-		it 'can be deleted' do
-			@post = FactoryGirl.create(:post) 
-			visit posts_path
+    it 'has a list of posts' do
+      post1 = FactoryGirl.build_stubbed(:post)
+      post2 = FactoryGirl.build_stubbed(:second_post)
+      visit posts_path
+      expect(page).to have_content(/Rationale|content/)
+    end
 
-			click_link("delete_post_#{@post.id}_from_index")
-			expect(page.status_code).to eq(200)
-		end
-	end
+    it 'has a scope so that only post creators can see their posts' do
+      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
 
-	describe 'creation' do
-	  before do
-	    visit new_post_path
-	  end
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
 
-		it 'has an new form that can be reached' do			
-			expect(page.status_code).to eq(200)
-		end
-		it 'can be created from new form page' do
-			fill_in 'post[date]', with: Date.today
-			fill_in 'post[rationale]', with: "Some rationale"
-			click_on "Save"
-			expect(page).to have_content("Some rationale")
-	end
+      visit posts_path
 
-		it 'will have a user associated with it' do
-			fill_in 'post[date]', with: Date.today
-			fill_in 'post[rationale]', with: "User Association"
-			click_on 'Save'
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
+  end
 
-			expect(User.last.posts.last.rationale).to eq("User Association")
-		end
-	end
-	
-	describe 'edit' do
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      # TODO refactor
+      @post.update(user_id: @user.id)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'creation' do
+  	before do
+  		visit new_post_path
+  	end
+
+  	it 'has a new form that can be reached' do
+  		expect(page.status_code).to eq(200)
+  	end
+
+  	it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
+  	end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
+  describe 'edit' do
     before do
       @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
       login_as(@edit_user, :scope => :user)
@@ -97,16 +114,3 @@ describe 'navigate' do
     end
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
