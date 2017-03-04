b14a5e2bd88b81c8a6523cefbaed5bdabc85f24f
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-03-03 22:54:41 -0800

    Updated the audit log factory and tested out 6 day default

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :audit_log do
-    user nil
+    user
     status 1
-    start_date "2017-03-03"
-    end_date "2017-03-03"
+    start_date (Date.today - 6.days)
+    end_date nil
   end
 end
